6b12c3b8c7f6c8544767ad661562bf1f36ee27f1
TSwimson <TannerSwenson@gmail.com>
2014-01-07 11:10:09 -0800

    Revised reverse_string.rb

--- week1/day1/reverse_string.rb
+++ week1/day1/reverse_string.rb
@@ -1,10 +1,5 @@
 puts "Enter a string to be reversed "
 words = gets.chomp.split " "
-words.each do |word|
-    for i in 0..word.length/2
-        word[i], word[-i-1] = word[-i-1], word[i]
-    end
-end
-puts words
+words.each { |word| (0..word.length/2).each { |i| word[i], word[-i-1] = word[-i-1], word[i] } }
 words.each { |x| print x + " " }
 puts "\n"
