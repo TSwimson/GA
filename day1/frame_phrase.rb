0dbc3941524dd995e8b64fd4457ff533b0818c3d
TSwimson <TannerSwenson@gmail.com>
2014-01-07 09:43:12 -0800

    Initial commit with day1 exercises

--- day1/frame_phrase.rb
+++
@@ -1,9 +0,0 @@
-puts "Enter a phrase to be framed"
-words = gets.chomp.split " "
-
-longest_word_length = 0
-words.each { |w| longest_word_length = w.length if w.length > longest_word_length }
-width = longest_word_length + 4
-puts "*"*width
-words.each { |w| puts "*" + w.center(width - 2) + "*"}
-puts "*"*width
