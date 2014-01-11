9c45c593ba273adab85d969b0466aa12ef1a769e
TSwimson <TannerSwenson@gmail.com>
2014-01-10 18:14:59 -0800

    Prompt user for name age etc for animals and clients

--- week1/HappiTails/shelter.rb
+++ week1/HappiTails/shelter.rb
@@ -1,7 +1,8 @@
 # The shelter should display all the clients.
 # The shelter should display all the animals.
 # The shelter should display all the animals
-
+require "./animal.rb"
+require "./client.rb"
 class Shelter
     attr_accessor :clients, :animals
 
@@ -10,4 +11,15 @@ class Shelter
         @animals = []
     end
 
+    def add_animal
+        puts "Enter the name, age, gender, and species for the animal"
+        info = gets.chomp.split
+        @animals << Animal.new(info[0], info[1], info[2], info[3])
+    end
+
+    def add_client
+        puts "Enter the name, age, number of children, and number of pets for the client"
+        info = gets.chomp.split
+        @clients << Client.new(info[0], info[1], info[2], info[3])
+    end
 end
