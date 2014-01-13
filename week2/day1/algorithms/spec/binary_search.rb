54e4d694d35cc039dd07fcb8a7b261b781617210
TSwimson <TannerSwenson@gmail.com>
2014-01-13 15:13:08 -0800

    added looping binary search

--- week2/day1/algorithms/spec/binary_search.rb
+++ week2/day1/algorithms/spec/binary_search.rb
@@ -1,9 +1,12 @@
 def binary_search(array, target, start=0, ending=array.length)
+
   if start >= ending
     return nil
   end
+
   middle_index = ((ending - start)/2) + start
   middle_element = array[middle_index]
+
   if middle_element == target
     return middle_index
   elsif middle_element > target
@@ -11,4 +14,23 @@ def binary_search(array, target, start=0, ending=array.length)
   elsif middle_element < target
     return binary_search(array, target, middle_index + 1, ending)
   end
+
+end
+
+def binary_search_loop(array, target, start=0, ending=array.length)
+
+  while start < ending
+    middle_index = ((ending - start)/2) + start
+    middle_element = array[middle_index]
+
+    if middle_element == target
+      return middle_index
+    elsif middle_element > target
+      return binary_search(array, target, start, middle_index)
+    elsif middle_element < target
+      return binary_search(array, target, middle_index + 1, ending)
+    end
+  end
+  return nil
+
 end
