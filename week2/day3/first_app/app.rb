6752eec020bcbae9ec32c3f1fce2bee26ef9e70e
TSwimson <TannerSwenson@gmail.com>
2014-01-15 14:00:41 -0800

    calculator webapp

--- week2/day3/first_app/app.rb
+++ week2/day3/first_app/app.rb
@@ -1,16 +1,32 @@
 require "sinatra"
 require "sinatra/reloader"
 
-MY_USERS = [{name: "Jon"}, {name: "Jane"}]
-
-get "/users" do
-    users = ""
-    MY_USERS.each { |u| users += "#{u[:name]} </br>"}
-
-    users
+get "/add/:a/:b" do
+    (params[:a].to_i + params[:b].to_i).to_s
 end
 
-get "/users/:id" do
-    "#{MY_USERS[params[:id].to_i][:name]}"
+get "/subtract/:a/:b" do
+    (params[:a].to_i - params[:b].to_i).to_s
 end
+
+get "/multiply/:a.*/:b.*" do
+    ((params[:a].to_f + params[:splat][0].to_f/10.0) * (params[:b].to_f + params[:splat][1].to_f/10.0)).to_s
+end
+
+get "/multiply/:a/:b.*" do
+    ((params[:a].to_f) * (params[:b].to_f + params[:splat][1].to_f/10.0)).to_s
+end
+
+get "/multiply/:a/:b" do
+    (params[:a].to_i * params[:b].to_i).to_s
+end
+
+get "/divide/:a/:b" do
+    (params[:a].to_i / params[:b].to_i).to_s if params[:b].to_i > 0
+end
+
+get "/add/*/*" do
+    params[:splat].inject(0) { |sum, x| sum + x.to_i }.to_s
+end
+
 
