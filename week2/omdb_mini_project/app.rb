d671c87b88f1f8f4ff56fab243fd806e4a59fff9
TSwimson <TannerSwenson@gmail.com>
2014-01-17 14:33:25 -0800

    Get movies

--- week2/omdb_mini_project/app.rb
+++ week2/omdb_mini_project/app.rb
@@ -2,13 +2,41 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
+require "pry"
+class Movie
 
-get '/' do
+    attr_reader :title, :year, :id, :ad
 
+    def initialize title, year, id
+        @title = title
+        @year = year
+        @id = id
+        @ad = nil
+    end
+
+    def get_additional_content
+        response = Typhoeus.get("http://www.omdbapi.com/", :params => {:i => @id})
+        result = JSON.parse(response.body)
+        @ad = result.select{|k,v| %w(Plot Rated Runtime Genre Director Actors Poster Metascore).include? k}
+    end
+end
+
+def get_movies search
+    response = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => params[:splat][0]})
+    result = JSON.parse(response.body)["Search"]
+    movies = {}
+    result.each { |m| movies[m["imdbID"]] = Movie.new(m["Title"], m["Year"], m["imdbID"]) }
+    movies.each { |k,v| v.get_additional_content }
+end
+
+get '/*' do
+    if params[:splat][0].size > 0
+        @movies = get_movies params[:splat][0].size
+        binding.pry
+    end
     erb :index
 end
 
 post "/" do
-    
-    redirect "/"
+    redirect "/#{params[:s]}"
 end
