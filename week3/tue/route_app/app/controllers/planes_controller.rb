441ad3abc0d2e6034678be85de5d923561e707ca
TSwimson <TannerSwenson@gmail.com>
2014-01-21 15:47:44 -0800

    Added ability to edit planes

--- week3/tue/route_app/app/controllers/planes_controller.rb
+++ week3/tue/route_app/app/controllers/planes_controller.rb
@@ -11,10 +11,35 @@ class PlanesController < ApplicationController
         render :new
     end
 
+    def show
+        plane_id = params[:id]
+        @plane = Plane.find(plane_id)
+        render :show
+    end
+
     def create
         plane = params.require(:plane).permit(:name, :engine_type, :description)
-        Plane.create(plane)
-        redirect_to "/planes"
+        new_plane = Plane.create(plane)
+        redirect_to "/planes/#{new_plane.id}"
+    end
+
+    def edit
+        plane_id = params[:id]
+        @plane = Plane.find(plane_id)
+        render :edit
+    end
+
+    def update
+        plane_id = params[:id]
+        plane = Plane.find(plane_id)
+
+        # get updated data
+        updated_attributes = params.require(:plane).permit(:name, :type, :description)
+        # update the plane
+        plane.update_attributes(updated_attributes)
+
+        #redirect to show
+        redirect_to "/planes/#{plane_id}"
     end
 
 end
