5ab8b1dc92fe4c96a29825467cf608362fb5d7c5
Tanner Swenson <TannerSwenson@gmail.com>
2014-01-27 11:01:05 -0800

    add annotate

--- week4/mon/wdi6_cook_book_blank/config/routes.rb
+++ week4/mon/wdi6_cook_book_blank/config/routes.rb
@@ -1,4 +1,14 @@
 CookBook::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/destroy"
+
+  get "users/new"
+
+  get "users/show"
+
+  get "users/create"
+
   resources :books, :recipes, :ingredients
 
   root to: 'books#index'
